e1d14c0a248030083ed969bd39c1cc6b0bcf1b95
BuildTools <unconfigured@null.spigotmc.org>
2024-05-27 14:33:27 +0300

    Changed implementation of default Hero class for future features

---
+++ ArenaGameEngine/Heroes/Protector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine.Heroes
+{
+    public class Protector : Hero
+    {
+
+        public int hitCount { get; private set; }
+        public double ArmorValue { get; set; }
+        public Protector(string name, double armor, double strenght, IWeapon weapon) 
+            : base(name, armor, strenght, weapon)
+        {
+            hitCount = 0;
+            ArmorValue = armor;
+        }
+
+        public override double Attack() {
+             return (Strenght + Weapon.AttackDamage)*0.8;
+        }
+
+        public override double Defend(double damage) {
+            hitCount = 1;
+            double weaponDefendPower = Weapon.BlockPower * 0.2;
+            double damageReceived = damage - (Armor + weaponDefendPower);
+            Armor = Armor - Armor * 0.1 * hitCount;
+            if (hitCount < 8)
+            {
+                hitCount++;
+            }
+            else {
+                Armor = ArmorValue;
+                hitCount = 0;
+            }
+            Health -= damageReceived;
+            return damageReceived;
+
+        }
+    }
+}
